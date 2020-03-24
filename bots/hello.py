262d740a53b6abe36b52709874c28d0bef278c02
tadeubanzato <tadeubanzato@gmail.com>
2020-03-24 18:33:28 -0400

    Update hello.py

--- bots/hello.py
+++ bots/hello.py
@@ -29,7 +29,7 @@ fillrows = len(sheet.get_all_values()) # Get total number of rolls with data add
 #talkeiMessage = sheet.cell(3, 1).value
 #randNumber = (random.randrange(fillrows))
 #print ("Total rows: " + randNumber)
-x = randint(1, randNumber)
+x = randint(1, fillrows)
 print (x)
 #x = randint(1, randNumber)
 
