668f876c4879f6b533557143b18bb72e76485a48
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 09:21:15 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -11,7 +11,8 @@ auth.set_access_token("1106313860460568576-wVk6Olx2T3dmwMB8A4iDGC7jmzWkhk",
     "9iGV5ruDnAw4bcTxf5Slpwu9NqvsugDSqJtHJXGJNTK4i")
 
 # Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
 
 logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger()
@@ -22,7 +23,7 @@ class FavRetweetListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
-        #print ("Processing tweet id: " + tweet.id)
+        print ("Processing tweet id " + tweet.id)
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
