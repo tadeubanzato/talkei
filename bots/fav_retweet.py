75fe18c97234efa0cc643500154b026cda85b382
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 09:56:47 -0400

    Revert "Update fav_retweet.py"
    This reverts commit 01a67cee58cc6e74757e5505bc8bec4ae1d92507.

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -23,7 +23,7 @@ class FavRetweetListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
-        print("Processing tweet id ", tweet.id)
+        logger.critical("Processing tweet id ", tweet.id)
         #logger.info("Processing tweet id ", tweet.id)
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
