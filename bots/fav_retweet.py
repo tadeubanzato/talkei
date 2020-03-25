26722805d0e8ce4d360d7d89ad1bc7656ba689e1
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 09:06:35 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -2,8 +2,17 @@
 
 import tweepy
 import logging
-from config import create_api
 import json
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("i0fnpu89sMI8QMnyGKHJkdyYS",
+    "ruWDxELm9PSAwnbrz6PcxZ7TFaPfQqPeoLn7g2rYuN2PsRisyv")
+auth.set_access_token("1106313860460568576-wVk6Olx2T3dmwMB8A4iDGC7jmzWkhk",
+    "9iGV5ruDnAw4bcTxf5Slpwu9NqvsugDSqJtHJXGJNTK4i")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -14,7 +23,7 @@ class FavRetweetListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
-        logger.info(f"Processing tweet id {tweet.id}")
+        logger.info("Processing tweet id: " + tweet.id)
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
@@ -36,10 +45,10 @@ class FavRetweetListener(tweepy.StreamListener):
         logger.error(status)
 
 def main(keywords):
-    api = create_api()
+    #api = create_api()
     tweets_listener = FavRetweetListener(api)
     stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["en"])
+    stream.filter(track=keywords, languages=["pt"])
 
 if __name__ == "__main__":
-    main(["Python", "Tweepy"])
+    main(["Bolsonazi", "biroliro", "bolsonaro imbecil", "#BolsonaroGenocida", "#Bolsonaroacabou", "#ForaBolsonaro", "#BolsonaroNaoEmaisPresidente"])
