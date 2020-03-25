d97c7b1b818aae589089140ba518d89f465a7904
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 09:11:14 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -23,7 +23,7 @@ class FavRetweetListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
-        logger.info("Processing tweet id: " + tweet.id)
+        print ("Processing tweet id: " + tweet.id)
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
