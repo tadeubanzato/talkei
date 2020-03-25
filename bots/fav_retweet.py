b7b96b66cf6b36966c8040c361913d072ee32eab
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 12:25:09 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -58,16 +58,16 @@ class FavRetweetListener(tweepy.StreamListener):
                 #tweet.user.follow()
                 if not tweet.user.following:
                     tweet.user.follow()
-                        def countdown(t):
-                            while t:
-                                mins, secs = divmod(t, 60)
-                                timer = '{:02d}:{:02d}'.format(mins, secs)
-                                print(timer)
-                                time.sleep(1)
-                                t -= 1
-                            print('Restart')
-                            print("Following: ",tweet.user)
-                        t = (15 * 30)
+                    def countdown(t):
+                        while t:
+                            mins, secs = divmod(t, 60)
+                            timer = '{:02d}:{:02d}'.format(mins, secs)
+                            print(timer)
+                            time.sleep(1)
+                            t -= 1
+                        print('Restart')
+                        print("Following: ",tweet.user)
+                    t = (15 * 30)
                     return
                     # print("Following: ",tweet.user)
                     # print(")
