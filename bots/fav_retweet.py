e1299771437d0f68744532a54984f8e9cb5ee859
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 15:41:09 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -28,20 +28,6 @@ class FavRetweetListener(tweepy.StreamListener):
         logger.error(status)
 
     def on_status(self, tweet):
-        def limit_handled(cursor):
-            while True:
-                try:
-                    yield cursor.next()
-                except tweepy.RateLimitError:
-                    print('ENTRANDO ESTADO DE ESPERA - API LIMIT')
-                    t = (15 * 60)
-                    while t:
-                        mins, secs = divmod(t, 60)
-                        timer = '{:02d}:{:02d}'.format(mins, secs)
-                        print(timer)
-                        time.sleep(1)
-                        t -= 1
-
         time.sleep(15 * 60)
         # t = (3)
         # while t:
