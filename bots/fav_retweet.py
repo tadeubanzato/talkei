35df597edd5b29e4f4b9e558eb53437c9787f200
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 12:08:28 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -62,7 +62,7 @@ class FavRetweetListener(tweepy.StreamListener):
                         while t:
                             mins, secs = divmod(t, 60)
                             timer = '{:02d}:{:02d}'.format(mins, secs)
-                            print(timer, end="\r")
+                            print(timer)
                             time.sleep(1)
                             t -= 1
                         print('Restart')
