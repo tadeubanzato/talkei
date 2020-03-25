ed793ee3e7439ea01906e1ff9c4032eba3f686a7
tadeubanzato <tadeubanzato@gmail.com>
2020-03-25 10:05:05 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -39,7 +39,7 @@ class FavRetweetListener(tweepy.StreamListener):
             # Retweet, since we have not retweeted it yet
             try:
                 tweet.retweet()
-                tweet.user.follow()
+                #tweet.user.follow()
                 #print("Following user: ",tweet.user)
             except Exception as e:
                 logger.error("Error on fav and retweet", exc_info=True)
