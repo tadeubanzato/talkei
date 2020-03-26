9e5b4e72886abb3edfb6ecad2b298f2b9a7b19e6
tadeubanzato <tadeubanzato@gmail.com>
2020-03-26 14:00:35 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -7,12 +7,10 @@ import json
 import time
 
 """
-
 This script is for listening Twitter timeline and:
 1. Favorite all twits with the comments based on the search keywords
 2. Retweet any twits with the search keywords criteria
 3. Follow any users with that twitted with the search criteria
-
 """
 
 # Authenticate to Twitter
@@ -23,11 +21,10 @@ auth.set_access_token("1106313860460568576-wVk6Olx2T3dmwMB8A4iDGC7jmzWkhk",
 
 # Create color code
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
@@ -62,7 +59,7 @@ class FavRetweetListener(tweepy.StreamListener):
                 while t:
                     mins, secs = divmod(t, 60)
                     timer = '{:02d}:{:02d}'.format(mins, secs)
-                    print(bcolors.FAIL + "API rest on error 1, restarting in:" + bcolors.ENDC, timer, end="\r")
+                    print(bcolors.RED + "Restart API Tweep rest 1 in:" + bcolors.ENDC, timer, end="\r")
                     time.sleep(1)
                     t -= 1
 
@@ -71,24 +68,16 @@ class FavRetweetListener(tweepy.StreamListener):
             try:
                 tweet.retweet()
                 if not tweet.user.following:
-                    print(bcolors.OKBLUE + "Following user: " + bcolors.ENDC,tweet.user.name)
+                    print(bcolors.BLUE + "Following user: " + bcolors.ENDC,tweet.user.name)
                     #time.sleep(60 * 15)
-                    t=(10)
-                    while t:
-                        mins, secs = divmod(t, 60)
-                        timer = '{:02d}:{:02d}'.format(mins, secs)
-                        print(bcolors.FAIL + "API rest on follow, restarting in:" + bcolors.ENDC, timer, end="\r")
-                        time.sleep(1)
-                        t -= 1
+                    # t=(10)
+                    # while t:
+                    #     mins, secs = divmod(t, 60)
+                    #     timer = '{:02d}:{:02d}'.format(mins, secs)
+                    #     print(bcolors.RED + "API Follow restart in:" + bcolors.ENDC, timer, end="\r")
+                    #     time.sleep(1)
+                    #     t -= 1
 
-            except tweepy.TweepError:
-                t=(60 * 15)
-                while t:
-                    mins, secs = divmod(t, 60)
-                    timer = '{:02d}:{:02d}'.format(mins, secs)
-                    print(bcolors.FAIL + "API rest on error 2, restarting in:" + bcolors.ENDC, timer, end="\r")
-                    time.sleep(1)
-                    t -= 1
 
             except Exception as e:
                 logger.error("Error on fav and retweet", exc_info=True)
@@ -99,12 +88,22 @@ class FavRetweetListener(tweepy.StreamListener):
 
 def main(keywords):
 
-    # Create API connection
-    api = tweepy.API(auth, wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
-    tweets_listener = FavRetweetListener(api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["pt"])
+    try:
+        # Create API connection
+        api = tweepy.API(auth, wait_on_rate_limit=True,
+            wait_on_rate_limit_notify=True)
+        tweets_listener = FavRetweetListener(api)
+        stream = tweepy.Stream(api.auth, tweets_listener)
+        stream.filter(track=keywords, languages=["pt"])
+
+    except tweepy.TweepError:
+        t=(60 * 15)
+        while t:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(bcolors.RED + "Restart API back in:" + bcolors.ENDC, timer, end="\r")
+            time.sleep(1)
+            t -= 1
 
 if __name__ == "__main__":
     main(["#ForaBolsonaro", "#BolsonaroGenocida", "#BolsoNazi", "#Bolsonaroacabou", "#BolsonaroNaoEmaisPresidente", "biroliro", "bolsonaro imbecil"])
