5f83f0663286f97fc5faee34f9b89da38bb9d9c1
tadeubanzato <tadeubanzato@gmail.com>
2020-03-30 10:24:59 -0400

    Update minions_reply.py

--- bots/minions_reply.py
+++ bots/minions_reply.py
@@ -36,9 +36,6 @@ logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger()
 
 class FavRetweetListener(tweepy.StreamListener):
-    print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
-    print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
-
     def __init__(self, api):
         self.api = api
         self.me = api.me()
@@ -46,18 +43,23 @@ class FavRetweetListener(tweepy.StreamListener):
     def on_error(self, status):
         logger.error(status)
 
+    def on_status(self, tweet):
+        print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
+        print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
 
-    api.update_status("Esse cara é um loco.")
-    user_name = "talkei2019"
-    tweets = api.user_timeline(screen_name=user_name)
-    firt_tweet = tweets[0]
-    print(firt_tweet.text)
-    print("done")
-    api.update_status('@{} louco genocida.'.format(user_name), firt_tweet.id)
+        user_name = "talkei2019"
+        tweets = api.user_timeline(screen_name=user_name)
+        firt_tweet = tweets[0]
+        print(firt_tweet.text)
+        print("done")
+        api.update_status('@{} Esse cara é uma piada #Genocida #ForaBolsonaro'.format(user_name), firt_tweet.id)
 
-
-
-
+    # except Exception as e:
+    #     logger.error("Error on fav and retweet", exc_info=True)
 
     def on_error(self, status):
         logger.error(status)
@@ -71,13 +73,14 @@ def main(keywords):
         tweets_listener = FavRetweetListener(api)
         stream = tweepy.Stream(api.auth, tweets_listener)
         stream.filter(track=keywords, languages=["pt"])
+        reply_new_tweets()
 
     except tweepy.TweepError:
         t=(60 * 15)
         while t:
             mins, secs = divmod(t, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(bcolors.RED + "Restart API back in:" + bcolors.ENDC, timer, end="\r")
+            print(bcolors.RED + "Restart API back in:" + bcolors.ENDC, timer, "\r")
             time.sleep(1)
             t -= 1
 
