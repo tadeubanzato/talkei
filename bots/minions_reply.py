5118ee19218a56d619df877b39aa96c908152e35
tadeubanzato <tadeubanzato@gmail.com>
2020-03-29 22:16:49 -0400

    Update minions_reply.py

--- bots/minions_reply.py
+++ bots/minions_reply.py
@@ -47,15 +47,6 @@ class FavRetweetListener(tweepy.StreamListener):
     def on_status(self, tweet):
         print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
-
-        api.update_status(
-            status="Esse presidente é um bossal, sem mais. #ForaBolsonaro #Bolsonazi #BolsonaroGenocida",
-            in_reply_to_status_id=tweet.id,
-        )
 
 
     def on_error(self, status):
