fef061870a32fdc3c18055fe41eb9f22a0c8a9b6
tadeubanzato <tadeubanzato@gmail.com>
2020-03-30 16:20:31 -0400

    Update script_checker.py

--- bots/script_checker.py
+++ bots/script_checker.py
@@ -4,7 +4,14 @@ from subprocess import Popen
 
 for process in psutil.process_iter():
     if process.cmdline() == ['python3', 'fav_retweet.py ']:
-        sys.exit('Process found: exiting.')
+        print('FAV Runing')
+    else Popen(['python3', 'fav_retweet.py'])
 
-print('Process not found: starting it.')
-Popen(['python3', 'fav_retweet.py'])
+    if process.cmdline() == ['python3', 'minions_reply_OLD.py']:
+        print('Minions Runing')
+    else Popen(['python3', 'minions_reply_OLD.py']
+# 
+# sys.exit('Process found: exiting.')
+#
+# print('Process not found: starting it.')
+# Popen(['python3', 'fav_retweet.py'])
