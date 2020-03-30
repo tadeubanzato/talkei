094ac1dde005b651863e5302753aed181e4258e2
tadeubanzato <tadeubanzato@gmail.com>
2020-03-30 09:43:21 -0400

    Update minions_reply.py

--- bots/minions_reply.py
+++ bots/minions_reply.py
@@ -51,7 +51,6 @@ class FavRetweetListener(tweepy.StreamListener):
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
             return
-        #MensagemMinion = "Esse presidente é um bossal, sem mais. #ForaBolsonaro #Bolsonazi #BolsonaroGenocida"
         user_name =  "talkei2019"
         tweets = api.user_timeline(screen_name=user_name)
         firt_tweet = tweets[0]
