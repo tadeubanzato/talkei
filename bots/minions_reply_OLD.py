20e5c271cc541f4484c595772263fefe762f58d4
tadeubanzato <tadeubanzato@gmail.com>
2020-03-30 12:04:00 -0400

    Update minions_reply_OLD.py

--- bots/minions_reply_OLD.py
+++ bots/minions_reply_OLD.py
@@ -31,13 +31,6 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-frases = ['Votou nesse energúmeno assina em baixo as mortes #Genocida #ForaBolsonaro #bolsoNazi',
-            'Não tem como defender os indefensável #Genocida #ForaBolsonaro #bolsoNazi',
-            'Impossível apoiar um genocida seja ele quem for #Genocida #ForaBolsonaro #bolsoNazi',
-            'Não vamos virar a Venezuela mas quem sabe a alemanha de 39 #Genocida #ForaBolsonaro #bolsoNazi',
-            'Incapaz, acéfalo, genocida, imvecí são os adjetivos para o Bolsonaro #Genocida #ForaBolsonaro #bolsoNazi']
-
-
 api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
 
@@ -56,6 +49,12 @@ class FavRetweetListener(tweepy.StreamListener):
 
         print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
+        frases = ['Votou nesse energúmeno assina em baixo as mortes #Genocida #ForaBolsonaro #bolsoNazi',
+                    'Não tem como defender os indefensável #Genocida #ForaBolsonaro #bolsoNazi',
+                    'Impossível apoiar um genocida seja ele quem for #Genocida #ForaBolsonaro #bolsoNazi',
+                    'Não vamos virar a Venezuela mas quem sabe a alemanha de 39 #Genocida #ForaBolsonaro #bolsoNazi',
+                    'Incapaz, acéfalo, genocida, imvecí são os adjetivos para o Bolsonaro #Genocida #ForaBolsonaro #bolsoNazi']
+                    
         m = random.choice(frases)
         #m = 'Votou no Bolsonaro também assina os óbitos desse energúmeno #Bolsonazi #Genocida #ForaBolsonaro'  # our status message
         if tweet.in_reply_to_status_id is not None or \
