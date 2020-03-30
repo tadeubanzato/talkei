4718ec500a1d8a9a0f7c83f7dc9ba870ddc3896e
tadeubanzato <tadeubanzato@gmail.com>
2020-03-30 16:13:47 -0400

    Update script_checker.py

--- bots/script_checker.py
+++ bots/script_checker.py
@@ -6,11 +6,11 @@ for process in psutil.process_iter():
     if process.cmdline() == ['python3', 'minions_reply_OLD.py']:
         print('Minions Runing Well')
     else print ("Restarting Minions")
-        Popen(['python', 'your_script.py'])ls
+        Popen(['python3', 'your_script.py'])ls
 
     if process.cmdline() == ['python3', 'fav_retweet.py']:
         print('FAV Runing Well')
     else print("Restarting FAV")
-        Popen(['python', 'fav_retweet.py'])ls
+        Popen(['python3', 'fav_retweet.py'])ls
 
 sys.exit('Good Job!')
