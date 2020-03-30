802c22e6816e138a69e506707a049e3308b35f00
tadeubanzato <tadeubanzato@gmail.com>
2020-03-29 22:03:14 -0400

    Update minions_reply.py

--- bots/minions_reply.py
+++ bots/minions_reply.py
@@ -36,10 +36,7 @@ logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger()
 
 class FavRetweetListener(tweepy.StreamListener):
-    # Create API connection
-    api = tweepy.API(auth, wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
-        
+
     def __init__(self, api):
         self.api = api
         self.me = api.me()
@@ -48,6 +45,9 @@ class FavRetweetListener(tweepy.StreamListener):
         logger.error(status)
 
     def on_status(self, tweet):
+        # Create API connection
+        api = tweepy.API(auth, wait_on_rate_limit=True,
+            wait_on_rate_limit_notify=True)
         print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
         if tweet.in_reply_to_status_id is not None or \
