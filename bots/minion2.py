e99b8b9edfd0185a069db76dbd7b8623a11443cd
tadeubanzato <tadeubanzato@gmail.com>
2020-04-01 12:57:20 -0400

    Update minion2.py

--- bots/minion2.py
+++ bots/minion2.py
@@ -50,8 +50,8 @@ class FavRetweetListener(tweepy.StreamListener):
 
         print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
-        # check = tweet.text
-        # print(check[:2])
+        check = tweet.text
+        print(check[:2])
         lines = open('frases.txt').read().splitlines()
         m = random.choice(lines)
 
@@ -61,7 +61,7 @@ class FavRetweetListener(tweepy.StreamListener):
             return
         #print(tweepy.TweepError)
 
-        if tweepy.TweepError:
+        if not tweet.retweet or tweepy.TweepError:
             try:
                 print(bcolors.RED + "RESPONDENDO: ",m,bcolors.ENDC)
                 sn = tweet.user.screen_name
