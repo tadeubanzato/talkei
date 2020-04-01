9ab572bf78670672714f932e7988d2bb37e64e7c
tadeubanzato <tadeubanzato@gmail.com>
2020-04-01 12:39:54 -0400

    Update minion2.py

--- bots/minion2.py
+++ bots/minion2.py
@@ -60,7 +60,7 @@ class FavRetweetListener(tweepy.StreamListener):
             return
         #print(tweepy.TweepError)
 
-        if not tweet.retweet or tweepy.TweepError:
+        if tweet.text [0:2] is "RT" or not tweet.retweet or tweepy.TweepError:
             try:
                 print(bcolors.RED + "RESPONDENDO: ",m,bcolors.ENDC)
                 sn = tweet.user.screen_name
