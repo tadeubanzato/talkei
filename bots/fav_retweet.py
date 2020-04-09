78a3d4999387e65cc45be3a27ea7fd868ddcfdf0
tadeubanzato <tadeubanzato@gmail.com>
2020-04-09 17:49:52 -0400

    Update fav_retweet.py

--- bots/fav_retweet.py
+++ bots/fav_retweet.py
@@ -44,11 +44,11 @@ class FavRetweetListener(tweepy.StreamListener):
         logger.error(status)
 
     def on_status(self, tweet):
-        t=(60 * 2)
+        t=(60 * 1)
         while t:
             mins, secs = divmod(t, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(bcolors.RED + "Restart API back in:" + bcolors.ENDC, timer, "\r")
+            print(bcolors.RED + "Restart API back in:" + bcolors.ENDC, timer, end="\r")
             time.sleep(1)
             t -= 1
         print(bcolors.GREEN + "Processing tweet id: " + bcolors.ENDC, tweet.id)
