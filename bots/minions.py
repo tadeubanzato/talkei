391006e71a6dbaf31864ac0e3ca7775c45f21fd4
tadeubanzato <tadeubanzato@gmail.com>
2020-04-10 09:03:58 -0400

    Update minions.py

--- bots/minions.py
+++ bots/minions.py
@@ -47,14 +47,6 @@ class FavRetweetListener(tweepy.StreamListener):
         logger.error(status)
 
     def on_status(self, tweet):
-        t=(60 * 1)
-        while t:
-            mins, secs = divmod(t, 60)
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(bcolors.RED + "Restart API back in:" + bcolors.ENDC, timer, end="\r")
-            time.sleep(1)
-            t -= 1
-
         print(bcolors.GREEN + "Processing tweet id:" + bcolors.ENDC, tweet.id)
         print(bcolors.BLUE + "Message:", tweet.text, bcolors.ENDC)
         lines = open('frases.txt').read().splitlines()
