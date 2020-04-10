3fd697bf1b58fe8fb814d5df558746585c7a09e2
tadeubanzato <tadeubanzato@gmail.com>
2020-04-10 14:10:56 -0400

    Update minions_scrapping.py

--- bots/minions_scrapping.py
+++ bots/minions_scrapping.py
@@ -52,14 +52,12 @@ class TweetListener(tweepy.StreamListener):
         check = tweet.text
         if check[:2] == "RT":
             flagNew = "Retweet"
-            print("RT")
         else:
             flagNew = "NEW TWEET"
-            print("NEW")
 
-        minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':['https://twitter.com/'+tweet.user.screen_name],'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text], 'New Tweet':flagNew})
+        minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':['https://twitter.com/'+tweet.user.screen_name],'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text],'Tweet Link':twtLink,'New Tweet':flagNew})
         df = DataFrame(minions)
-        df.to_csv ('/home/pi/talkei/minions_log.csv', columns=['Tweet ID', 'User Name', 'User URL', 'Friend Counts', 'Followers', 'Created', 'Location','Tweet','New Tweet'], encoding='utf-8', index=False, header=None, mode='a') # here you have to write path, where result file will be stored
+        df.to_csv ('/home/pi/talkei/minions_log.csv', columns=['Tweet ID', 'User Name', 'User URL', 'Friend Counts', 'Followers', 'Created', 'Location','Tweet','Tweet Link','New Tweet'], encoding='utf-8', index=False, header=None, mode='a') # here you have to write path, where result file will be stored
 
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
