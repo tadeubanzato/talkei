51b6ed3e3ae3720b834e5c69e4e475a9657959a8
tadeubanzato <tadeubanzato@gmail.com>
2020-04-10 11:46:14 -0400

    Update minions_scrapping.py

--- bots/minions_scrapping.py
+++ bots/minions_scrapping.py
@@ -6,7 +6,7 @@ import logging
 import json
 import time
 import os
-import pandas as pd
+from pandas import DataFrame
 
 """
 This script is for listening Twitter timeline and:
@@ -49,20 +49,18 @@ class TweetListener(tweepy.StreamListener):
         print(bcolors.GREEN + "Tweet from: " + bcolors.ENDC, tweet.user.name)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC,"\n")
 
-        df = pd.DataFrame ({
-            'Tweet ID':tweet.id,
-            'User Name':tweet.user.screen_name,
-            'User URL':tweet.user.url,
-            'Friends Count':tweet.user.friends_count,
-            'Followers Count':tweet.user.followers_count,
-            'Timezone':tweet.user.time_zone,
-            'Created at':tweet.user.created_at,
-            'Location':tweet.user.location,
-            'Tweet':tweet.text,
-        },
-        index=['Tweet ID', 'User Name','User URL','Friends Count','Followers Count','Timezone', 'Created at', 'Location', 'Tweet'])
-
-        df.to_csv('/home/pi/talkei/minions_log.csv', encoding='utf-8', index=true)
+        minions = {'Tweet ID':tweet.id,
+                    'User Name':tweet.user.screen_name,
+                    'User URL':tweet.user.url,
+                    'Friends Count':tweet.user.friends_count,
+                    'Followers Count':tweet.user.followers_count,
+                    'Timezone':tweet.user.time_zone,
+                    'Created at':tweet.user.created_at,
+                    'Location':tweet.user.location,
+                    'Tweet':tweet.text,
+                    }
+        df = DataFrame(minions, columns=['Tweet ID', 'User Name','User URL','Friends Count','Followers Count','Timezone', 'Created at', 'Location', 'Tweet'])
+        df.to_csv (r'/home/pi/talkei/minions_log.csv', index = None, header=True) # here you have to write path, where result file will be stored
 
 
         # # Write a new roll with the information on the CSV file
