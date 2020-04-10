e5f1e3ee343c6726ea6619d6d7463b54f7593cc0
tadeubanzato <tadeubanzato@gmail.com>
2020-04-10 14:14:16 -0400

    Update minions_scrapping.py

--- bots/minions_scrapping.py
+++ bots/minions_scrapping.py
@@ -48,17 +48,18 @@ class TweetListener(tweepy.StreamListener):
     def on_status(self, tweet):
         print(bcolors.GREEN + "Tweet from: " + bcolors.ENDC, tweet.user.name)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC,"\n")
-        twtLink = ('https://twitter.com/',tweet.user.id,'/status/',tweet.id)
+        twtLink = ('https://twitter.com/'+tweet.user.id+'/status/'+tweet.id)
+        print(twtLink)
         check = tweet.text
         if check[:2] == "RT":
-            flagNew = "Retweet"
+            flagNew = "RT"
         else:
             flagNew = "NEW TWEET"
 
         minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':['https://twitter.com/'+tweet.user.screen_name],'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text],'Tweet Link':twtLink,'New Tweet':flagNew})
         df = DataFrame(minions)
-        df.to_csv ('/home/pi/talkei/minions_log.csv', columns=['Tweet ID', 'User Name', 'User URL', 'Friend Counts', 'Followers', 'Created', 'Location','Tweet','Tweet Link','New Tweet'], encoding='utf-8', index=False, header=None, mode='a') # here you have to write path, where result file will be stored
-
+        df.to_csv ('/home/pi/talkei/minions_log.csv', encoding='utf-8', index=False, header=None, mode='a') # here you have to write path, where result file will be stored
+        #columns=['Tweet ID', 'User Name', 'User URL', 'Friend Counts', 'Followers', 'Created', 'Location','Tweet','Tweet Link','New Tweet'],
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it porque vc nao ta atualizando?
