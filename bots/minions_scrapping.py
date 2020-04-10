8acffd706fc80067b9ea079e53a833d264acbd1c
tadeubanzato <tadeubanzato@gmail.com>
2020-04-10 14:20:44 -0400

    Update minions_scrapping.py

--- bots/minions_scrapping.py
+++ bots/minions_scrapping.py
@@ -48,7 +48,8 @@ class TweetListener(tweepy.StreamListener):
     def on_status(self, tweet):
         print(bcolors.GREEN + "Tweet from: " + bcolors.ENDC, tweet.user.name)
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC,"\n")
-        twtLink = ('https://twitter.com/'+tweet.user.id+'/status/'+tweet.id)
+        twtUserLink = 'https://twitter.com/' + str(tweet.user.id)
+        twtLink =  twtUserLink + '/status/' + tweet.user.screen_name
         print(twtLink)
         check = tweet.text
         if check[:2] == "RT":
@@ -56,7 +57,8 @@ class TweetListener(tweepy.StreamListener):
         else:
             flagNew = "NEW TWEET"
 
-        minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':['https://twitter.com/'+tweet.user.screen_name],'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text],'Tweet Link':twtLink,'New Tweet':flagNew})
+        #minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':['https://twitter.com/'+tweet.user.screen_name],'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text],'Tweet Link':twtLink,'New Tweet':flagNew})
+        minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':twtUserLink,'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text],'Tweet Link':twtLink,'New Tweet':flagNew})
         df = DataFrame(minions)
         df.to_csv ('/home/pi/talkei/minions_log.csv', encoding='utf-8', index=False, header=None, mode='a') # here you have to write path, where result file will be stored
         #columns=['Tweet ID', 'User Name', 'User URL', 'Friend Counts', 'Followers', 'Created', 'Location','Tweet','Tweet Link','New Tweet'],
