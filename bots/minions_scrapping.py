8ce037a46f01153fe65902077c274d2d4ddd8c2a
tadeubanzato <tadeubanzato@gmail.com>
2020-04-10 12:40:09 -0400

    Update minions_scrapping.py

--- bots/minions_scrapping.py
+++ bots/minions_scrapping.py
@@ -51,7 +51,7 @@ class TweetListener(tweepy.StreamListener):
 
         minions =({'Tweet ID':[tweet.id],'User Name':[tweet.user.screen_name],'User URL':['https://www.twitter.com/'+tweet.user.screen_name],'Friend Counts':[tweet.user.friends_count],'Followers':[tweet.user.followers_count],'Created':[tweet.user.created_at],'Location':[tweet.user.location],'Tweet':[tweet.text]})
         df = DataFrame(minions)
-        df.to_csv ('/home/pi/talkei/minions_log.csv', index=False, header=False, mode='a') # here you have to write path, where result file will be stored
+        df.to_csv ('/home/pi/talkei/minions_log.csv', encoding='utf-8', index=False, header=False, mode='a') # here you have to write path, where result file will be stored
 
 
         # # Write a new roll with the information on the CSV file
