7b318ae2ab0f4c00a955fd16a8107876bdcd78c0
tadeubanzato <tadeubanzato@gmail.com>
2020-04-11 15:26:44 -0400

    Update ping_runing.py

--- bots/ping_runing.py
+++ bots/ping_runing.py
@@ -3,21 +3,21 @@
 # bots/ping_runing.py
 import requests
 import sys
-
+runing = sys.argv[1]
 ## IFTTT and Webhook alet
 # Array for the values on Webhook ["value1"], ["value2"], etc
 report = {}
 
 # Webhook will receive IP address from the pi
-report["value1"] = "Starting script: "
+report["value1"] = "Starting script: " + runing
 
 # Get DashPi IP address
 # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # s.connect(("8.8.8.8", 80))
-#s.close()
-runing = sys.argv[1]
-print (runing)
-report["value2"] = runing
+# s.close()
+# runing = sys.argv[1]
+#print (runing)
+#report["value2"] = runing
 
 # Resquest post to Webhook integrated with IFTTT
 requests.post('https://maker.ifttt.com/trigger/Talkei/with/key/d1oS5w-uq90y8fCs2ot5qG', data=report)
