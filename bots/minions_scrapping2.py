20f602b0b5f40445ca0ce5adac474e30d79b25e7
tadeubanzato <tadeubanzato@gmail.com>
2020-04-14 14:49:48 -0400

    Update minions_scrapping2.py

--- bots/minions_scrapping2.py
+++ bots/minions_scrapping2.py
@@ -41,7 +41,7 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/talkei/bots/talkei-0c766b314509.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open('MinionsCount').sheet1 # discover total rows on sheet
-print(client.open('MinionsCount').sheet1)
+print(len(sheet.get_all_values()))
 
 
 # Create LOGGER object
