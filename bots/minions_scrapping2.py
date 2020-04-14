d18a3c121c26a6e472d56c48be6ece546de5f011
tadeubanzato <tadeubanzato@gmail.com>
2020-04-14 14:36:41 -0400

    Update minions_scrapping2.py

--- bots/minions_scrapping2.py
+++ bots/minions_scrapping2.py
@@ -62,7 +62,7 @@ class TweetListener(tweepy.StreamListener):
         print(bcolors.BLUE + "Message: ", tweet.text, bcolors.ENDC)
         twtLink =  'https://twitter.com/' + tweet.user.screen_name + '/status/' + str(tweet.id)
         print(twtLink,"\n")
-        print(tweet.status.coordinates)
+        print(tweet.coordinates)
 
         if tweet.in_reply_to_status_id is not None or \
             tweet.user.id == self.me.id:
@@ -78,7 +78,7 @@ class TweetListener(tweepy.StreamListener):
 
         print(bcolors.RED + "Writing data to Sheets" + bcolors.ENDC)
         # Write data on Google Sheets
-        row = [tweet.user.screen_name,tweet.user.friends_count,tweet.user.followers_count,tweet.user.created_at,tweet.user.location,status.user.coordinates,flagNew,'https://twitter.com/' + tweet.user.screen_name,twtLink]
+        row = [tweet.user.screen_name,tweet.user.friends_count,tweet.user.followers_count,tweet.user.created_at,tweet.user.location,flagNew,'https://twitter.com/' + tweet.user.screen_name,twtLink]
         index = index + 1
         sheet.insert_row(row, index)
 
