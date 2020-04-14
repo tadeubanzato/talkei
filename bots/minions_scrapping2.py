e90a7bef8cf7141498a6e56b949034bf36b5929b
tadeubanzato <tadeubanzato@gmail.com>
2020-04-14 14:53:26 -0400

    Update minions_scrapping2.py

--- bots/minions_scrapping2.py
+++ bots/minions_scrapping2.py
@@ -81,7 +81,7 @@ class TweetListener(tweepy.StreamListener):
         print(bcolors.RED + "Writing data to Sheets" + bcolors.ENDC)
         userLink = 'https://twitter.com/' + tweet.user.screen_name
         # Write data on Google Sheets
-        row = [tweet.user.screen_name,tweet.user.friends_count,tweet.user.followers_count,tweet.user.created_at,tweet.user.location,flagNew,userLink,twtLink]
+        row = [tweet.user.screen_name,tweet.user.friends_count,tweet.user.followers_count,tweet.user.created_at]
         index += 1
         sheet.insert_row(row, index)
 
