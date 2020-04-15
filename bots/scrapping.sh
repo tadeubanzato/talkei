29e3eb4b6774bc42116fee946e2d593c9253aa8b
tadeubanzato <tadeubanzato@gmail.com>
2020-04-14 20:30:29 -0400

    Update scrapping.sh

--- bots/scrapping.sh
+++ bots/scrapping.sh
@@ -4,8 +4,8 @@ var1=`ps aux | grep minions_scrapping2.py | grep -v grep | wc -l`
 echo $var1
 if [ $var1 -eq 1 ]
 then
-  echo "The Process is Running"
+  echo "The Process is Running, good buy."
 else
-  echo "Starting process"
+  echo "Starting process now!"
   python3 /home/pi/talkei/bots/minions_scrapping2.py &
 fi
