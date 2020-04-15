eb9ce33fbebed23dca0c435b4c36b6d5427c2a8c
tadeubanzato <tadeubanzato@gmail.com>
2020-04-14 20:29:06 -0400

    Update scrapping.sh

--- bots/scrapping.sh
+++ bots/scrapping.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-var1=`ps -ef | grep -v grep | grep minions_scrapping2.py | awk '{print $2}'`
+var1=`ps aux | grep minions_scrapping2.py | grep -v grep | wc -l`
 echo $var1
-if [ -n $var1 ]
+if [ $var1 -eq 1 ]
 then
-echo "The Process is Running"
+  echo "The Process is Running"
 else
-echo "Starting process"
-python3 /home/pi/talkei/bots/minions_scrapping2.py &
+  echo "Starting process"
+  python3 /home/pi/talkei/bots/minions_scrapping2.py &
 fi
