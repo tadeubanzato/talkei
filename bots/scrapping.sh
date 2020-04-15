58bff321966098086dcfb9eacc1b2b2455a8a7ab
tadeubanzato <tadeubanzato@gmail.com>
2020-04-14 20:23:35 -0400

    Update scrapping.sh

--- bots/scrapping.sh
+++ bots/scrapping.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CHECK='ps aux | grep minions_scrapping2.py | grep -v grep | wc -l'
+CHECK='ps -aux | minions_scrapping2.py  | grep -v grep | wc -l'
 
-if [ $CHECK = 0 ]
+if [ $CHECK -eq 0 ]
   then
   	python3 /home/pi/talkei/bots/minions_scrapping2.py &
-    
+
 fi
